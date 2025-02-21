44b8682fdbfe9626c77cce75e72b629286721bcf
Akezhan Amanzhol <akezhanamanzhol@MacBook-Air-Akezhan.local>
2025-02-21 09:48:03 +0500

    Lab5

---
+++ labs/Lab5/1.py
@@ -0,0 +1,13 @@
+import re
+
+with open('input.txt', 'r') as file:
+    text = file.read()
+
+pattern = r"a*b"
+matches = re.findall(pattern, text)
+if matches:
+    print("Найденные совпадения:")
+    for match in matches:
+        print(match)
+else:
+    print("Совпадений не найдено")
